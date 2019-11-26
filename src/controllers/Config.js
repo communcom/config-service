0c8a59616e1ea0b05e0448ce0b11909abad077eb
Ilya Lebedev <bacherko@gmail.com>
2019-11-26 13:57:02 +0300

    fix: update version checker

--- src/controllers/Config.js
+++ src/controllers/Config.js
@@ -1,10 +1,20 @@
+const { get } = require('lodash');
 const core = require('cyberway-core-service');
 const { Basic } = core.controllers;
 const { isLowerThan } = require('../utils/versions');
 
+const minimalVersions = {
+    app: {
+        ios: '1.0.0',
+        android: '1.0.0',
+    },
+};
+
 class Config extends Basic {
     async getConfig({ platform, deviceType, type, isWebView, version }) {
-        if (platform === 'ios' && type === 'app' && isLowerThan(version, '1.0.0')) {
+        const minVersion = get(minimalVersions, [type, platform]);
+
+        if (minVersion && isLowerThan(version, minVersion)) {
             throw {
                 code: 5000,
                 message: 'Need update application version',
